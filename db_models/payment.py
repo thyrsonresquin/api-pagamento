e3480792477da4e66c71a8d4c940ec10eae20994
thyrsonresquin <thyrson@outlook.com>
2026-05-08 19:41:35 -0400

    modelo de pagamento database

--- db_models/payment.py
+++ db_models/payment.py
@@ -1,12 +1,21 @@
 from repository.database import db
 
+# modelo de pagamento, representando a tabela de pagamentos no banco de dados
 class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float, nullable=False)
     paid = db.Column(db.Boolean, default=False)
     bank_payment_id = db.Column(db.Integer, nullable=True)
     qr_code = db.Column(db.String(255), nullable=True)
-    expiration_data = db.Column(db.DateTime)
+    expiration_date = db.Column(db.DateTime)
 
-    def __repr__(self):
-        return f'<Payment {self.id} - {self.value} - {self.paid} - {self.bank_payment_id} - {self.qr_code} - {self.expiration_data}>'
+# método para converter o objeto em dicionário, facilitando a serialização para JSON
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "value": self.value,
+            "paid": self.paid,
+            "bank_payment_id": self.bank_payment_id,
+            "qr_code": self.qr_code,
+            "expiration_date": self.expiration_date
+        }
